0b2f050530392cfef37443543b087ef39c91ea7b
Michael Jach <miyu.46300@gmail.com>
2016-07-04 14:42:36 +0200

    Fabrication v0.0.1

---
+++ Generateur/Generateur/Fabrication.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generateur
+{
+    public class Fabrication
+    {
+
+        private string server;
+        private string userid;
+        private string password;
+        private string database;
+        private MySqlConnection connexion;
+
+
+        public Fabrication(string server, string userid, string password, string database)
+        {
+            this.server = server;
+            this.userid = userid;
+            this.password = password;
+            this.database = database;
+        }
+
+        public void ConnexionOpen()
+        {
+            String myConnectionString = "server=" + this.server + ";uid=" + this.userid + ";pwd=" + this.password + ";database=" + this.database + ";";
+            this.connexion = new MySql.Data.MySqlClient.MySqlConnection();
+
+            try
+            {
+                connexion.ConnectionString = myConnectionString;
+                connexion.Open();
+                Console.WriteLine("Connexion à la base de données établie !");
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Impossible de se connecter à la base de données !");
+            }
+        }
+
+        public void ConnexionClose()
+        {
+            try
+            {
+                this.connexion.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine("Impossible de se déconnecter de la base de données !");
+                Console.ReadLine();
+            }
+        }
+
+
+        public void GenererLigneProduction() {
+            
+
+
+            MySqlCommand cmd = this.connexion.CreateCommand();
+            Random rnd1 = new Random();
+
+            int i = rnd1.Next(1, 100);
+
+            // Requête SQL
+            cmd.CommandText = "SELECT NOM FROM nom WHERE ID = '" + i + "'";
+
+            // Exécution de la commande SQL
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                //Console.WriteLine(reader[0]+" " + reader[1]);
+                return reader[0] + "";
+            }
+            return null;
+        
+    }
+
+
+
+
+    }
+}
--- Generateur/Generateur/Program.cs
+++ Generateur/Generateur/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleApplication13
         public static void Main(string[] args)
         {
             Commande BDDcommande = new Commande("127.0.0.1", "root", "", "client");
+            Fabrication fabrication = new Fabrication("127.0.0.1", "root", "", "client");
             Console.ReadLine();
         }
 
