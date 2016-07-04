e550a4d2c051e4e4ddf39086cc850771dbf10b69
Newnewflo <minewemade@gmail.com>
2016-07-04 15:06:42 +0200

    Commande V0.2

--- Generateur/Generateur/Commande.cs
+++ Generateur/Generateur/Commande.cs
@@ -22,39 +22,18 @@ namespace Generateur
         public void GenererSatisfaction()
         {
             Random rnd1 = new Random();
-            Random rnd2 = new Random();
-            Random rnd3 = new Random();
-            Random rnd4 = new Random();
-
-            int packaging = rnd1.Next(0, 10);
-            int produit = rnd2.Next(0, 10);
-            int delais = rnd3.Next(0, 10);
-            int sav = rnd4.Next(0, 10);
-
 
             MySqlCommand cmd = bdd.connexion.CreateCommand();
 
             // Requête SQL
             cmd.CommandText = "INSERT INTO Satisfaction (Packaging,Produit,Delais_livraison,SAV) VALUES (@Packaging,@Produit,@Delais_livraison,@SAV)";
             // utilisation de l'objet contact passé en paramètre
-            cmd.Parameters.AddWithValue("@Packaging", packaging);
-            cmd.Parameters.AddWithValue("@Produit", produit);
-            cmd.Parameters.AddWithValue("@Delais_livraison", delais);
-            cmd.Parameters.AddWithValue("@SAV", sav);
+            cmd.Parameters.AddWithValue("@Packaging", rnd1.Next(0, 10));
+            cmd.Parameters.AddWithValue("@Produit", rnd1.Next(0, 10));
+            cmd.Parameters.AddWithValue("@Delais_livraison", rnd1.Next(0, 10));
+            cmd.Parameters.AddWithValue("@SAV", rnd1.Next(0, 10));
 
-            for (int i=0;i<20;i++)
-            {
-
-                packaging = rnd1.Next(0, 10);
-                produit = rnd2.Next(0, 10);
-                delais = rnd3.Next(0, 10);
-                sav = rnd4.Next(0, 10);
-                // Exécution de la commande SQL
-                cmd.ExecuteNonQuery();
-            }
-            
-
-            bdd.ConnexionClose();
+            cmd.ExecuteNonQuery();
         }
 
         public void GenererSatisfait()
